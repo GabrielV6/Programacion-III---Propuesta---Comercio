4b524d3270eff109714368af6394428ea2f52aad
tfdace <acedoestefania@gmail.com>
2022-12-10 19:47:21 -0300

    se actualiza stock y proveedor en Articulos luego de compras

--- TP-Comercio-P3/tp-comercio-Vargas-Delgado/FormularioRegistroCompra.aspx.cs
+++ TP-Comercio-P3/tp-comercio-Vargas-Delgado/FormularioRegistroCompra.aspx.cs
@@ -12,7 +12,7 @@ namespace tp_comercio_Vargas_Delgado
     public partial class FormularioRegistroCompra : System.Web.UI.Page
     {
         public List<Registro> ListaRegistro { get; set; }
-        
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["usuariologueado"] == null)
@@ -32,7 +32,7 @@ namespace tp_comercio_Vargas_Delgado
                         TotalCompra.Text = "Monto total por venta: $0";
 
                     }
-                    
+
                     ArticuloNegocio articuloNegocio = new ArticuloNegocio();
                     List<Articulo> listaArticulo = articuloNegocio.listar();
 
@@ -51,7 +51,7 @@ namespace tp_comercio_Vargas_Delgado
 
                     dgvRegistroCompra.DataSource = Session["ListaCompra"];
                     dgvRegistroCompra.DataBind();
-                  
+
                     List<Registro> Lista = (List<Registro>)Session["ListaCompra"];
 
                     Nullable<decimal> totalCompra = 0;
@@ -89,7 +89,7 @@ namespace tp_comercio_Vargas_Delgado
                 }
             }
         }
-        
+
         protected void btnAgregarCompra_Click(object sender, EventArgs e)
         {
 
@@ -99,7 +99,7 @@ namespace tp_comercio_Vargas_Delgado
             // el registro.Id sera igual al valor que tiene la sesscion en ID  +1 
 
             registro.Id = Session["ListaCompra"] != null ? ((List<Registro>)Session["ListaCompra"]).Count + 1 : 1;
-         
+
             // TODO: Si necesitamos algun otro dato debemos agregarlo al registro (bien sea de proveedor o de articulo)
 
             registro.Tipo = compra;
@@ -109,15 +109,15 @@ namespace tp_comercio_Vargas_Delgado
             registro.MontoTotal = registro.Cantidad * registro.Monto;
 
             int IdArticulo = int.Parse(ddlArticulo.SelectedValue);
-            
+
             registro.proveedor = new Proveedor();
             List<Registro> Lista = (List<Registro>)Session["ListaCompra"];
-            
+
             //revisar si el proveedor es el mismo que el anterior
 
             if (Session["ListaCompra"] != null)
             {
-               
+
                 if (Lista.Count > 0)
                 {
                     if (Lista[Lista.Count - 1].proveedor.Id == int.Parse(ddlProveedor.SelectedValue))
@@ -134,23 +134,23 @@ namespace tp_comercio_Vargas_Delgado
                 }
                 else
                 {
-                    
+
                     registro.proveedor.Id = int.Parse(ddlProveedor.SelectedValue);
                     registro.proveedor.RazonSocial = ddlProveedor.SelectedItem.Text;
                     registro.Destinatario = int.Parse(ddlProveedor.SelectedValue);
                 }
             }
-        
+
 
             registro.articulo = new Articulo();
             registro.articulo.Nombre = ddlArticulo.SelectedItem.Text;
             registro.articulo.Id = int.Parse(ddlArticulo.SelectedValue);
-            
+
             Lista.Add(registro);
 
             //grisar el ddlProveedor que no permita cambiarlo una vez se selecciono uno en la lista
 
-            
+
             //llamar al page load
             Response.Redirect("FormularioRegistroCompra.aspx");
 
@@ -175,25 +175,27 @@ namespace tp_comercio_Vargas_Delgado
 
             registroNegocio.agregar(Lista);
 
+            //actualiza stock en articulos
+
+            foreach (Registro registro in Lista)
+            {
+                ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+                Articulo articulo = new Articulo();
+                // busca el stock anterior del producto
+                List<Articulo> articulos = new List<Articulo>();
+                articulos = articuloNegocio.listaParaEditar(registro.articulo.Id);
+                articulo.Id = registro.articulo.Id;
+                // se suma el stock anterior a la cantidad comprada
+                articulo.Stock = articulos[0].Stock + registro.Cantidad;
+                // actualiza el ultimo proveedor
+                Proveedor proveedor = new Proveedor();
+                articulo.proveedor = proveedor;
+                articulo.proveedor.Id = registro.Destinatario;
+                articuloNegocio.modificarPorCompra(articulo);
+            }
+
             Session.Remove("ListaCompra");
             Response.Redirect("WebVerRegistroCompra.aspx");
-            /*
-            ArticuloNegocio articuloNegocio = new ArticuloNegocio();
-            Articulo articulo = new Articulo();
-            // busca el stock anterior del producto
-            List<Articulo> articulos = new List<Articulo>();
-            articulos = articuloNegocio.listaParaEditar(registro.articulo.Id);
-            articulo.Id = registro.articulo.Id;
-            // se suma el stock anterior a la cantidad comprada
-            articulo.Stock = articulos[0].Stock + registro.Cantidad;
-            // actualiza el ultimo proveedor
-            Proveedor proveedor = new Proveedor();
-            articulo.proveedor = proveedor;
-            articulo.proveedor.Id = registro.Destinatario;
-            articuloNegocio.modificarPorCompra(articulo);
-            */
-
-            Response.Redirect("WebVerRegistroCompra.aspx", false);
         }
 
         protected void btnEliminar_Click(object sender, EventArgs e)
@@ -231,7 +233,7 @@ namespace tp_comercio_Vargas_Delgado
             Session.Remove("ListaCompra");
             Session.Add("ListaCompra", ListaAux);
             Response.Redirect("FormularioRegistroCompra.aspx");
-            
+
             //RegistroNegocio negocio = new RegistroNegocio();
             ////negocio.eliminar(IdRegistro);
             //Response.Redirect("WebVerRegistroCompra.aspx");
